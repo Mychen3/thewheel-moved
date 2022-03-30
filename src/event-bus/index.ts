449bdd5059750840ef66c4071d616e269dea833f
chenzhilong <97491530@qq.com>
2022-03-30 17:50:22 +0800

    添加取消订阅， 存储订阅换成对象，添加id做为key来找回调函数

--- src/event-bus/index.ts
+++ src/event-bus/index.ts
@@ -1,45 +1,68 @@
- type ICallbackList = Array<Function>
+ interface ICallbackObject {
+    [id:string]:Function
+ }
 
   interface IEventObject {
-    [eventName:string]:ICallbackList
+    [eventName:string]:ICallbackObject
   }
 
   interface IEventBus {
     publish<T extends any[]>(eventName: string, args: T): void;
-    subscribe(eventName:string,callback:Function):void;
+    subscribe(eventName:string,callback:Function):{unSubscribe:Function};
   }
 
   class EventBus implements IEventBus {
        private _eventObject:IEventObject;
+       private _callbackId:number
 
       constructor() {
           this._eventObject = {}
+          this._callbackId = 0
       }
 
      // 订阅事件
-    subscribe(eventName:string,callback:Function){
+    subscribe(eventName:string,callback:Function):{unSubscribe:Function} {
 
         // 先初始化事件，先判断是否有该事件
-      if (!this._eventObject[eventName]) {
-        // 使用对象存储，注销回调函数的时候提高删除的效率
-        this._eventObject[eventName] = []
-      }
-      this._eventObject[eventName].push(callback)
+        if (!this._eventObject[eventName]) {
+            // 使用对象存储，注销回调函数的时候提高删除的效率
+            this._eventObject[eventName] = {}
+        }
+
+        // 设置回调函数唯一id
+        let id: number = this._callbackId++
+
+        // 存储订阅者的回调函数
+        // callbackId使用后需要自增，供下一个回调函数使用
+        this._eventObject[eventName][id] = callback
+
+        // 每一次订阅事件 也会生成取消订阅的函数
+        const unSubscribe = () => {
+
+         delete this._eventObject[eventName][id];
+
+           // 如果这个事件没有订阅者了，也把整个事件对象清除
+             if (Object.keys(this._eventObject[eventName]).length === 0){
+                   delete this._eventObject[eventName]
+             }
+        };
+        return {unSubscribe}
     }
+
 
     // 发布事件
     publish<T extends any[]>(eventName:string,...args:T){
-      // 取出当前事件所有的回调函数list
-          const callBackList:Array<Function> = this._eventObject[eventName]
+      // 取出当前事件所有的回调函数obj
+          const callBackObject = this._eventObject[eventName]
 
            // 边缘case 如果没有当前列表
-           if (!callBackList) {
+           if (!callBackObject) {
              throw new TypeError("no current callback")
            }
 
            // 然后去执行每一个回调函数
-         for (let callBack of callBackList){
-               callBack(args)
+         for (let id in callBackObject){
+               callBackObject[id](...args)
          }
     }
   }
--- test/event-bus.test.ts
+++ test/event-bus.test.ts
@@ -3,18 +3,38 @@ import EventBus from '../src/event-bus/index'
 
 describe('hallo eventBus',()=>{
 
-    it('simple eventBus ',  ()=> {
+    it.skip('simple eventBus ',  ()=> {
             const eventBus = new EventBus()
             // 订阅事件
-           eventBus.subscribe('eventTest1',(param)=>{
+           eventBus.subscribe('eventTest1',(param:number)=>{
                console.log('打印出来了1',param)
            })
-           eventBus.subscribe('eventTest1',(param )=>{
+           eventBus.subscribe('eventTest1',(param:number)=>{
                console.log('打印出来了2',param)
            })
               eventBus.publish("eventTest1",66)
-
     });
+
+
+    it('clear 取消订阅',  ()=> {
+        const eventBus = new EventBus()
+        // 订阅事件
+        eventBus.subscribe('eventTest1',(param:number)=>{
+            console.log('我订阅1',param)
+        })
+        let  subscriber2 = eventBus.subscribe('eventTest1',(param:number)=>{
+            console.log('我订阅2',param)
+        })
+        eventBus.subscribe('eventTest1',(param:number)=>{
+            console.log('我订阅3',param)
+        })
+
+        // 我这边就取消订阅了
+        subscriber2.unSubscribe()
+
+        eventBus.publish("eventTest1",66)
+    });
+
 })
 
 
